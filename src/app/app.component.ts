b465a67b1178d7246868b484f56815a6567c1c70
Bartłomiej Bluszcz <GFGexample@gmail.orgg>
2023-09-20 15:59:46 +0200

    lalal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,15 @@ import {SwPush, SwUpdate} from "@angular/service-worker";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
+readonly VAPID_PUBLIC_KEY = 'BLnVk1MBGFBW4UxL44fuoM2xxQ4o9CuxocVzKn9UVmnXZEyPCTEFjI4sALMB8qN5ee67yZ6MeQWjd5iyS8lINAg';
 
-
-    constructor(private swUpdate: SwUpdate) {
+    constructor(private swUpdate: SwUpdate,  private swPush: SwPush) {
 
     }
 
     ngOnInit() {
 
+      console.log("this.swUpdate ", this.swUpdate);
         if (this.swUpdate.isEnabled) {
 
             this.swUpdate.available.subscribe(() => {
@@ -31,6 +32,31 @@ export class AppComponent  implements OnInit {
 
 
     }
+
+    subscribeToNotifications() {
+
+      this.swPush.requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC_KEY
+        })
+        .then(sub =>{
+        console.log("this.VAPID_PUBLIC_KEY ", this.VAPID_PUBLIC_KEY);
+
+        const key = sub.getKey('p256dh');
+        console.log("key encoded to base64 ", btoa(String.fromCharCode.apply(null, new Uint8Array(key))));
+        const auth = sub.getKey('auth');
+ console.log("auth ", auth);
+
+        // this._webPushHttpService.subcribeToChromeNotifications(
+        //   sub.endpoint,
+        //   key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+        //   auth ? btoa(String.fromCharCode.apply(null, new Uint8Array(auth))) : ''
+        // )
+          // .subscribe()
+      }
+
+        )
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 
 }
 
